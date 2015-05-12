e016a29143881454f5f2a5ad582d0c63c03a5592
jagrutipatel <jagruti999@gmail.com>
2015-05-12 15:52:51 -0400

    Applied color change with prop() and toggleClass() methods.

--- public/jq_js.js
+++ public/jq_js.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 					var frt = this.nextSibling.innerHTML;
 					if (this.checked){
 						$("ul").append("<li>"+frt+"</li>");
+						$(this.nextSibling).toggleClass("fruits");
+						$('.fruits').prop("color","red");
 					}else{
 						$("ul li:contains("+frt+")").remove();
-					}
+						$(this.nextSibling).toggleClass("fruits");
+						}
 				});
 				$("div").on("click","button",function(event){
 					var fname = prompt("Enter fruit name:");
