318df3b3fed4df8288e5b5c691672264fcebdbeb
jagrutipatel <jagruti999@gmail.com>
2015-06-03 16:39:09 -0400

    product pg_handlebar

---
+++ public/javascripts/prodpg_hb.js
@@ -0,0 +1,56 @@
+
+		/*$( ".newitem" ).on( "click", ".glyphicon", function(){
+		var x =this;
+		console.log(x);
+	$.ajax({
+		url:"/products/"  + this.id,
+		type:"delete",
+		success:function(data){
+			 if (data.success == true){
+				$(x).parent("div").remove();
+    }
+	}
+});
+});	
+	*/
+$.ajax({
+		url:"/products/",
+		type:"get",
+		success:function(data){
+		for(var i=0;i<data.length;i++){
+			var test=Handlebars.compile($('#hbar').html());
+					$('.newitem').append(test(data[i]));
+				}
+			}
+		})
+		
+$(document).ready(function(){
+		 $(".addbtn").on('click',function(event){
+			console.log($(".addbtn"));
+			event.preventDefault();
+			//console.log("c");
+				var item=$('input:text[name="itemname"]').val();
+				var des=$('input:text[name="description"]').val();
+				var p= $('input:text[name="price"]').val();
+							
+			$.ajax({
+				url:"/products",
+				type:"post",
+				data:{
+						itemname:item,
+						description:des,
+						price:p,
+				},
+				dataType:'json',
+				
+				success:function(data){	
+				console.log(data);
+					var test=Handlebars.compile($('#hbar').html());
+					$('.newitem').append(test(data));
+				}
+			})
+		})
+});
+
+
+
